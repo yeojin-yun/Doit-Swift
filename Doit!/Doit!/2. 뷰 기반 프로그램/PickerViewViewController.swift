0487c0ec98ad71dfe607f70357b8163f01b98726
순진이 <sail041@naver.com>
2021-12-18 21:25:32 +0900

    AlertControl

--- Doit!/Doit!/2. 뷰 기반 프로그램/PickerViewViewController.swift
+++ Doit!/Doit!/2. 뷰 기반 프로그램/PickerViewViewController.swift
@@ -84,36 +84,48 @@ extension PickerViewViewController {
     }
 }
 
+//두 번째 미션 - component 두개로 나눠 0번 컴포넌트에서 선택된 건 레이블에, 오른쪽 컴포넌트는 이미지뷰에 나올 수 있도록
 extension PickerViewViewController: UIPickerViewDelegate, UIPickerViewDataSource {
     //피커뷰에게 컴포넌트의 수를 정수 값으로 넘겨주는 델리게이트 메서드
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
+        return 2
     }
     
     //피커 뷰에게 컴포넌트의 열의 개수를 정수 값으로 넘겨주는 델리게이트 메서드
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return images.count
+        switch component {
+        case 0:
+            return images.count
+        case 1:
+            return images.count
+        default:
+            return 0
+        }
     }
     
-    //피커 뷰에게 컴포넌트의 각 열의 타이틀을 문자열(String) 값으로 넘겨주는 델리게이트 메서드
-//    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-//        <#code#>
-//    }
+
     
     func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
-        
-        let imageView = UIImageView()
-        imageView.image = images[row % images.count]
-        imageView.contentMode = .scaleAspectFit
-        
-        return imageView
+
+            let imageView = UIImageView()
+            imageView.image = images[row % images.count]
+            imageView.contentMode = .scaleAspectFit
+            return imageView
     }
 
     
     //사용자가 선택한 피커 뷰의 row를 사용하여 원하는 일을 하게하는 델리게이트 메서드
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        selectedImgView.image = images[row]
-        titleLbl.text = img[row]
+        switch component {
+        case 0:
+            titleLbl.text = img[row]
+        case 1:
+            selectedImgView.image = images[row]
+        default:
+            break
+        }
+//        selectedImgView.image = images[row]
+//        titleLbl.text = img[row]
     }
     
     
@@ -123,3 +135,80 @@ extension PickerViewViewController: UIPickerViewDelegate, UIPickerViewDataSource
     }
     
 }
+//첫 번째 실습
+//extension PickerViewViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+//    //피커뷰에게 컴포넌트의 수를 정수 값으로 넘겨주는 델리게이트 메서드
+//    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+//        return 1
+//    }
+//
+//    //피커 뷰에게 컴포넌트의 열의 개수를 정수 값으로 넘겨주는 델리게이트 메서드
+//    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+//        return images.count
+//    }
+//
+//    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+//
+//        let imageView = UIImageView()
+//        imageView.image = images[row % images.count]
+//        imageView.contentMode = .scaleAspectFit
+//
+//        return imageView
+//    }
+//
+//
+//    //사용자가 선택한 피커 뷰의 row를 사용하여 원하는 일을 하게하는 델리게이트 메서드
+//    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+//        selectedImgView.image = images[row]
+//        titleLbl.text = img[row]
+//    }
+//
+//
+//    //피커 뷰 row의 높이
+//    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+//        return 100.0
+//    }
+//
+//}
+
+//첫 번째 미션 - component 두개로 나눠 선택된 이미지가 레이블 및 이미지뷰에 나올 수 있도록
+//extension PickerViewViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+//    //피커뷰에게 컴포넌트의 수를 정수 값으로 넘겨주는 델리게이트 메서드
+//    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+//        return 2
+//    }
+//
+//    //피커 뷰에게 컴포넌트의 열의 개수를 정수 값으로 넘겨주는 델리게이트 메서드
+//    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+//        switch component {
+//        case 0:
+//            return images.count
+//        case 1:
+//            return images.count
+//        default:
+//            return 0
+//        }
+//    }
+//
+//
+//
+//    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+//
+//            let imageView = UIImageView()
+//            imageView.image = images[row % images.count]
+//            imageView.contentMode = .scaleAspectFit
+//            return imageView
+//    }
+//
+//
+//    //사용자가 선택한 피커 뷰의 row를 사용하여 원하는 일을 하게하는 델리게이트 메서드
+//    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+//        selectedImgView.image = images[row]
+//        titleLbl.text = img[row]
+//    }
+//
+//
+//    //피커 뷰 row의 높이
+//    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+//        return 100.0
+//    }
