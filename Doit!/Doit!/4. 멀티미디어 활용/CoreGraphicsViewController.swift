42212f31f94e9341104313a570c6f8ff8e00df48
순진이 <sail041@naver.com>
2022-01-16 21:07:01 +0900

    Core Graphics

--- Doit!/Doit!/4. 멀티미디어 활용/CoreGraphicsViewController.swift
+++ Doit!/Doit!/4. 멀티미디어 활용/CoreGraphicsViewController.swift
@@ -28,18 +28,177 @@ class CoreGraphicsViewController: UIViewController {
 //MARK: -Event
 extension CoreGraphicsViewController {
     @objc func drawBtnTapped(_ sender: UIButton) {
+        // context의 크기를 이미지뷰의 크기와 똑같이 생성
+        UIGraphicsBeginImageContext(imgView.frame.size)
         
+        // context생성 : 일종의 도화지
+        let context = UIGraphicsGetCurrentContext()
+        
+        //📍Drwa Line
+        // 선의 굵기
+        context?.setLineWidth(2.0)
+        // 선의 색상
+        context?.setStrokeColor(UIColor.red.cgColor)
+        // 선의 시작점
+        context?.move(to: CGPoint(x: 70, y: 70))
+        // 선의 도착점
+        context?.addLine(to: CGPoint(x: 270, y: 250))
+        
+        // 시작점과 도착점의 경로에 따라 그림을 그림
+        context?.strokePath()
+        
+        
+        //📍Drwa Triangle
+        // 선의 굵기
+        context?.setLineWidth(4.0)
+        // 선의 색상
+        context?.setStrokeColor(UIColor.blue.cgColor)
+        // 선의 시작점
+        context?.move(to: CGPoint(x: 170, y: 200))
+        // 선의 도착점
+        context?.addLine(to: CGPoint(x: 270, y: 350))
+        context?.addLine(to: CGPoint(x: 70, y: 350))
+        context?.addLine(to: CGPoint(x: 170, y: 200))
+        // 시작점과 도착점의 경로에 따라 그림을 그림
+        context?.strokePath()
+        
+        // 현재 context에 그려진 이미지를 가지고 와서 이미지 뷰에 나타냄
+        imgView.image = UIGraphicsGetImageFromCurrentImageContext()
+        
+        // 그림 그리기 끝내기
+        UIGraphicsEndImageContext()
     }
+    
+    
     @objc func rectangleBtnTapped(_ sender: UIButton) {
+        // context의 크기를 이미지뷰의 크기와 똑같이 생성
+        UIGraphicsBeginImageContext(imgView.frame.size)
+        
+        // context생성 : 일종의 도화지
+        let context = UIGraphicsGetCurrentContext()
+        
+        //📍Drwa Rectangle
+        // 선의 굵기
+        context?.setLineWidth(2.0)
+        // 선의 색상
+        context?.setStrokeColor(UIColor.green.cgColor)
+        // 사각형 위치 및 크기
+        context?.addRect(CGRect(x: 50, y: 50, width: 50, height: 50))
+        // 사각형 그리기
+        context?.strokePath()
+        
+        // 현재 context에 그려진 이미지를 가지고 와서 이미지 뷰에 나타냄
+        imgView.image = UIGraphicsGetImageFromCurrentImageContext()
+        
+        // 그림 그리기 끝내기
+        UIGraphicsEndImageContext()
         
     }
     @objc func circleBtnTapped(_ sender: UIButton) {
+        // context의 크기를 이미지뷰의 크기와 똑같이 생성
+        UIGraphicsBeginImageContext(imgView.frame.size)
         
+        // context생성 : 일종의 도화지
+        let context = UIGraphicsGetCurrentContext()
+        
+        //📍Drwa Ellipse(타원)
+        context?.setLineWidth(2.0)
+        context?.setStrokeColor(UIColor.magenta.cgColor)
+        context?.addEllipse(in: CGRect(x: 50, y: 50, width: 50, height: 200))
+        context?.strokePath()
+        
+        
+        //📍Drwa Circle
+        context?.setLineWidth(5.0)
+        context?.setStrokeColor(UIColor.white.cgColor)
+        context?.addEllipse(in: CGRect(x: 70, y: 200, width: 200, height: 200))
+        context?.strokePath()
+        
+        // 현재 context에 그려진 이미지를 가지고 와서 이미지 뷰에 나타냄
+        imgView.image = UIGraphicsGetImageFromCurrentImageContext()
+        
+        // 그림 그리기 끝내기
+        UIGraphicsEndImageContext()
     }
+    
+    
     @objc func arcBtnTapped(_ sender: UIButton) {
+        // context의 크기를 이미지뷰의 크기와 똑같이 생성
+        UIGraphicsBeginImageContext(imgView.frame.size)
         
+        // context생성 : 일종의 도화지
+        let context = UIGraphicsGetCurrentContext()
+        
+        //📍Drwa Arc1
+        context?.setLineWidth(5.0)
+        context?.setStrokeColor(UIColor.red.cgColor)
+        context?.move(to: CGPoint(x: 100, y: 50))
+        context?.addArc(tangent1End: CGPoint(x: 250, y: 50), tangent2End: CGPoint(x: 250, y: 200), radius: CGFloat(50))
+        context?.addLine(to: CGPoint(x: 250, y: 200))
+        context?.strokePath()
+        
+        //📍Drwa Arc2
+        context?.setLineWidth(5.0)
+        context?.setStrokeColor(UIColor.yellow.cgColor)
+        context?.move(to: CGPoint(x: 100, y: 250))
+        context?.addArc(tangent1End: CGPoint(x: 270, y: 250), tangent2End: CGPoint(x: 100, y: 400), radius: CGFloat(20))
+        context?.addLine(to: CGPoint(x: 100, y: 400))
+        
+        context?.strokePath()
+        
+        // 현재 context에 그려진 이미지를 가지고 와서 이미지 뷰에 나타냄
+        imgView.image = UIGraphicsGetImageFromCurrentImageContext()
+        
+        // 그림 그리기 끝내기
+        UIGraphicsEndImageContext()
     }
+    
+    
     @objc func fillBtnTapped(_ sender: UIButton) {
+        // context의 크기를 이미지뷰의 크기와 똑같이 생성
+        UIGraphicsBeginImageContext(imgView.frame.size)
+        
+        // context생성 : 일종의 도화지
+        let context = UIGraphicsGetCurrentContext()
+        
+        //📍Drwa Rectangle
+        context?.setLineWidth(1.0)
+        context?.setStrokeColor(UIColor.yellow.cgColor)
+        context?.setFillColor(UIColor.yellow.cgColor)
+        
+        let rectangle = CGRect(x: 70, y: 50, width: 200, height: 100)
+        context?.addRect(rectangle)
+        context?.fill(rectangle)
+        context?.strokePath()
+        
+        
+        //📍Drwa Rectangle
+        context?.setLineWidth(1.0)
+        context?.setStrokeColor(UIColor.white.cgColor)
+        context?.setFillColor(UIColor.white.cgColor)
+        
+        let circle = CGRect(x: 70, y: 200, width: 200, height: 100)
+        context?.addEllipse(in: circle)
+        context?.fillEllipse(in: circle)
+        context?.strokePath()
+        
+        //📍Drwa Triangle
+        context?.setLineWidth(1.0)
+        context?.setStrokeColor(UIColor.red.cgColor)
+        context?.setFillColor(UIColor.red.cgColor)
+        
+        context?.move(to: CGPoint(x: 170, y: 350))
+        context?.addLine(to: CGPoint(x: 270, y: 450))
+        context?.addLine(to: CGPoint(x: 70, y: 450))
+        context?.addLine(to: CGPoint(x: 170, y: 350))
+        context?.fillPath()
+        context?.strokePath()
+        
+        // 현재 context에 그려진 이미지를 가지고 와서 이미지 뷰에 나타냄
+        imgView.image = UIGraphicsGetImageFromCurrentImageContext()
+        
+        // 그림 그리기 끝내기
+        UIGraphicsEndImageContext()
         
     }
 }
