1c7f6690d02a602e68781a00856aaf4c715b0f8a
순진이 <sail041@naver.com>
2022-01-20 23:17:57 +0900

    SwipeGesture start

--- Doit!/Doit!/5. 이벤트와 제스쳐/SwipeGestureViewController.swift
+++ Doit!/Doit!/5. 이벤트와 제스쳐/SwipeGestureViewController.swift
@@ -9,21 +9,48 @@ import UIKit
 
 class SwipeGestureViewController: UIViewController {
 
+    let leftImg = UIView()
+    let topImg = UIView()
+    let rightImg = UIView()
+    let bottomImg = UIView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        configureUI()
         // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+//MARK: -UI
+extension SwipeGestureViewController {
+    final private func configureUI() {
+        setAttributes()
+        addTarget()
+        setConstraints()
     }
-    */
-
+    
+    final private func setAttributes() {
+        
+    }
+    
+    final private func addTarget() {
+        
+    }
+    
+    final private func setConstraints() {
+        let verticalStack = UIStackView(arrangedSubviews: [topImg, bottomImg])
+        verticalStack.axis = .vertical
+        verticalStack.distribution = .fillEqually
+        
+        let horizontalStack = UIStackView(arrangedSubviews: [leftImg, rightImg])
+        horizontalStack.axis = .horizontal
+        horizontalStack.distribution = .fillEqually
+        
+//        [<#UILabel#>, <#UIButton#>].forEach {
+//            view.addSubview($0)
+//            $0.translatesAutoresizingMaskIntoConstraints = false
+//        }
+    }
 }
