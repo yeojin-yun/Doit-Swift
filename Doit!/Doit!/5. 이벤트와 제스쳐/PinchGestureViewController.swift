5769ed92dc642c2b1e0bfa7cbd1a3fa812006a5d
순진이 <sail041@naver.com>
2022-01-25 00:05:05 +0900

    Pinch Gesture comment

--- Doit!/Doit!/5. 이벤트와 제스쳐/PinchGestureViewController.swift
+++ Doit!/Doit!/5. 이벤트와 제스쳐/PinchGestureViewController.swift
@@ -31,9 +31,12 @@ extension PinchGestureViewController {
     }
     
     @objc func doPinch(_ pinch: UIPinchGestureRecognizer) {
+        // pince 제스처의 상태 확인
         if (pinch.state == UIGestureRecognizer.State.began) {
+            // pinch제스쳐의 상태가 "시작"이면 앞에서 선언한 initialFontSize 변수에 현재 텍스트의 글자 크기 저장
             initialFontSize = pinchLabel.font.pointSize
         } else {
+            // pinch제스쳐의 상태가 "시작" 아니라면, 핀치제스쳐가 계속 진행되고 있는 상태이므로, initialFontSize에 저장해 둔 글자 크기 값에 scale 속성을 곱하여 텍스트의 글자 크기에 반영
             pinchLabel.font = pinchLabel.font.withSize(initialFontSize * pinch.scale)
         }
     }
